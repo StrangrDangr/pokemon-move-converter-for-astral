7b898967a085c4502d184569cd6268ea41c7d1ac
StrangrDangr <32497687+StrangrDangr@users.noreply.github.com>
2020-10-29 15:09:51 -0400

    Update scripts.js
    declared variables for each of the form inputs and set up title, display, and message variables to populate the output area. I did not start the dice roll section of message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-
+   $( "#submit" ).submit(function( event ){
+    event.preventDefault();
+    var name= $('#mname').val(),
+        desc= $('#desc').val(),
+        pow= $('#power').val(),
+        acc= $('#acc').val(),
+        pp= $('#pp').val(),
+        type= $('#type').val(),
+        cat= $('#cat').val(),
+        eff1= $('#effect1').val(),
+        prob1= $('#prob1').val(),
+        eff2= $('#effect2').val(),
+        prob2= $('#prob2').val(),
+        stab= $('#sametype').val(),
+        misc= $('#misc").val();
+     var title= "{name} uses " + name;
+     var display= name + " - " + type;
+     var message= "'Type: " + type "'<br />___<br />```<br />" + desc + "<br />```<br />";
+     $('#title').html(title);
+     $('#display').html(display);
+     $('#message').html(message);
+   });
 })
