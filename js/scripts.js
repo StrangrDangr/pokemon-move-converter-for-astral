5363a222adbbeef20377670837d0b95537e31e98
StrangrDangr <32497687+StrangrDangr@users.noreply.github.com>
2020-10-30 17:25:21 -0400

    Update scripts.js
    needs to be tested, but i think I've got everything we need here

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,9 @@ $(document).ready(function(){
         type= $('#type').val(),
         cat= $('#cat').val(),
         eff1= $('#effect1').val(),
-        prob1= $('#prob1').val(),
+        prob1= Math.ceil($('#prob1').val() / 10),
         eff2= $('#effect2').val(),
-        prob2= $('#prob2').val(),
+        prob2= Math.ceil($('#prob2').val() / 10),
         stab= $('#sametype').prop('checked'),
         crit= $('#crit-rate').prop('checked'),
         targ= $("input[name='target']:checked").val(),
@@ -38,14 +38,67 @@ $(document).ready(function(){
      };
      var title= "{name} uses " + name;
      var display= name + " - " + cat;
-     var toHit= if(acc>0){">To Hit: \{!(2d6) + " + acc + " + acc\} - evasion vs. 12 <br />"}else{"Auto success"};
+     var mech1= function(){
+        let x="<br>"
+        unless($(targ)=="default" && $(pri)=="default"){
+           if(pri != "default"){
+              x += ">" + $(pri) + "<br>";
+           };
+           If(targ!="default"){
+              x += ">" + $(targ) + "<br>";
+           };
+        };
+        return x;
+     };
+     var toHit= function(){
+        if(acc>0){
+           if($(crit)==false){
+              return ">To Hit: {!(2d6) + " + acc + " + acc} - evasion vs. 12 <br>";
+           }else{
+              return ">To Hit: !(2d6cr>10) + {acc + " + acc + "} - evasion vs. 12 <br>";
+           };
+        }elseif(pow>0){
+           return ">Auto success! !(2d6)<br>"
+        }else{
+           return ">Auto success!<br>"
+        };
+     };
      var dmg= function(){
         if(pow>0){
-           return ">Damage: \{!(1d4)" + convpow + " + " + cat + "\} vs " + defstat + ".<br>";
+           let d= "!(1d4)" + convpow + " + " + cat;
+           let e= "} vs " + defstat + ".<br>";
+           let c= ">Crit? + !!(1d4" + convpow + ") dmg.<br>";
+           if(stab==false){
+             return ">Damage: {" + d + e + c;
+           }else{
+             return ">Damage(STAB): {floor(1.5 *(" + d + "))" + e + c;
+           }
         }else{
            return " ";
         }};
-     var message= "'Type: " + type "'<br />" + pow + " Pow - " + acc + " Acc - " + pp + " pp<br />___<br />```<br />" + desc + "<br />```<br />" + toHit + dmg;
+     var extra= function(){
+        if (eff1 !=0){
+           let r1= "!!(1d10>" + (10-$(prob1)) + ")<br>";
+           let e1= ">" + eff1 + "? ";
+           if (eff2 !=0){
+              let r2= "!!(1d10>" + (10-$(prob2)) + ")<br>";
+              let e2= ">" + eff2 + "? ";
+              return e1+r1+e2+r2;
+           }else{
+              return e1+r1;
+           }
+        }else{
+           return " ";
+        }
+     };
+     var ms = function(){
+        if(misc!=0){
+           return ">" + misc;
+        }else{
+           return " ";
+        }
+     };
+     var message= "\'Type: " + type "\'<br />" + pow + " Pow - " + acc + " Acc - " + pp + " pp<br />___<br />```<br />" + desc + "<br />```" + mech1 + toHit + dmg + extra + ms;
      $('#title').html(title);
      $('#display').html(display);
      $('#message').html(message);
