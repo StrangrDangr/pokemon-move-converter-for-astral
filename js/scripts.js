bc0dcb7779441b8f5008df862044f3dd3f4e2b6a
StrangrDangr <32497687+StrangrDangr@users.noreply.github.com>
2020-10-30 15:13:38 -0400

    Update scripts.js
    included the new inputs and set up the to hit and damage lines for the message. Haven't set up how crit effects to hit and how stab effects the damage line. I'm setting up the different lines of the mechanic section as independent variables for readability.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     event.preventDefault();
     var name= $('#mname').val(),
         desc= $('#desc').val(),
-        pow= $('#power').val(),
-        acc= $('#acc').val(),
+        pow= Math.ceil($('#power').val() / 10),
+        acc= Math.ceil($('#acc').val() / 10),
         pp= $('#pp').val(),
         type= $('#type').val(),
         cat= $('#cat').val(),
@@ -12,11 +12,40 @@ $(document).ready(function(){
         prob1= $('#prob1').val(),
         eff2= $('#effect2').val(),
         prob2= $('#prob2').val(),
-        stab= $('#sametype').val(),
+        stab= $('#sametype').prop('checked'),
+        crit= $('#crit-rate').prop('checked'),
+        targ= $("input[name='target']:checked").val(),
+        pri= $("input[name='priority']:checked").val(),
         misc= $('#misc").val();
+     var convpow= function(){
+           let x= pow-4;
+           if (x==0){
+              return " ";
+           }elseif(x>0){
+              return " + " + x;
+           }else{
+              return " - " + (x * (-1));
+           };
+        };
+     var defstat= function(){
+        if (cat=="Atk"){
+           return "Def";
+        }elseif(cat=="SpAtk"){
+           return "Sp.Def";
+        }else{
+           return " ";
+        }
+     };
      var title= "{name} uses " + name;
-     var display= name + " - " + type;
-     var message= "'Type: " + type "'<br />___<br />```<br />" + desc + "<br />```<br />";
+     var display= name + " - " + cat;
+     var toHit= if(acc>0){">To Hit: \{!(2d6) + " + acc + " + acc\} - evasion vs. 12 <br />"}else{"Auto success"};
+     var dmg= function(){
+        if(pow>0){
+           return ">Damage: \{!(1d4)" + convpow + " + " + cat + "\} vs " + defstat + ".<br>";
+        }else{
+           return " ";
+        }};
+     var message= "'Type: " + type "'<br />" + pow + " Pow - " + acc + " Acc - " + pp + " pp<br />___<br />```<br />" + desc + "<br />```<br />" + toHit + dmg;
      $('#title').html(title);
      $('#display').html(display);
      $('#message').html(message);
