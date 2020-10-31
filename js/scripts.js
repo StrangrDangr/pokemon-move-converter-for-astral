ed88d621dcf693b1f9ffc3496fa010921dfeb832
StrangrDangr <32497687+StrangrDangr@users.noreply.github.com>
2020-10-31 01:22:01 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -37,14 +37,13 @@ $(document).ready(function(){
      };
      var title= "{name} uses " + name;
      var display= name + " - " + cat;
-      
-     var first= function(){
+     var first=function(){
         var fl= "<br>";
         if(pri != "default"){fl += ">" + pri + "<br>"};
         if(targ != "default"){fl += ">" + targ + "<br>"};
         return fl;
      };
-    var toHit= function(){
+    var toHit=function(){
         if(acc>0){
            if($(crit)==false){
               return ">To Hit: {!(2d6) + " + acc + " + acc} - evasion vs. 12 <br>";
